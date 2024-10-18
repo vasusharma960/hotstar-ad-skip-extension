adefdd443b184ce6662dbbc5154fdb7a6947cbf4
vasusharma960 <vasusharma.2058@gmail.com>
2024-10-18 17:53:55 +0530

    renamed backgroundURLCheck to service-worker

--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,26 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log("Background service worker installed.");
 });
 
-chrome.webNavigation.onHistoryStateUpdated.addListener(details => {
-    console.log("details ====> ", details);
-}, {
-  url: [{
-    hostContains: "hotstar.com",
-    pathContains: "watch"
-  }]
-}
+chrome.webNavigation.onHistoryStateUpdated.addListener(
+  (details) => {
+    console.log(details);
+    if (details.url.includes("/watch")) {
+      console.log("injecting script");
+      chrome.scripting
+        .executeScript({
+          target: { tabId: details.tabId },
+          files: ["detectAdsVideoObserver.js"],
+        })
+        .then(() => console.log("observer script injected"))
+        .catch((e) => console.error(e));
+    }
+  },
+  {
+    url: [
+      {
+        hostContains: "hotstar.com",
+        pathContains: "watch",
+      },
+    ],
+  }
 );
