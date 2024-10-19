1b9623b6b2e70e8922b18e6bc663f71580ef3bdd
vasusharma960 <vasusharma.2058@gmail.com>
2024-10-19 17:14:47 +0530

    added can and go back script to an IIFE for fixing multiple injections and executions of same script

--- cancelAndGoBackScript.js
+++ cancelAndGoBackScript.js
@@ -1,52 +1,54 @@
-const findBackIcon = () => {
-  const startTime = Date.now();
+(function () {
+  const findBackIcon = () => {
+    const startTime = Date.now();
 
-  const findBackIconInterval = setInterval(() => {
-    const backIconList = document.getElementsByClassName("icon-arrow-back");
+    const findBackIconInterval = setInterval(() => {
+      const backIconList = document.getElementsByClassName("icon-arrow-back");
 
-    if (backIconList && backIconList.length > 0) {
-      clearInterval(findBackIconInterval);
-      setTimeout(() => {
-        backIconList[0].click();
-        console.log("backIcon click success====> ");
-
+      if (backIconList && backIconList.length > 0) {
+        clearInterval(findBackIconInterval);
         setTimeout(() => {
-          console.log("backIcon find cancel btn====> ");
-          findAndClickCancelButton();
+          backIconList[0].click();
+          console.log("backIcon click success====> ");
+
+          setTimeout(() => {
+            console.log("backIcon find cancel btn====> ");
+            findAndClickCancelButton();
+          }, 1000);
         }, 1000);
-      }, 1000);
-    } else if (Date.now() > startTime) {
-      clearInterval(findBackIconInterval);
-      console.error("Back Icon not found.");
+      } else if (Date.now() > startTime) {
+        clearInterval(findBackIconInterval);
+        console.error("Back Icon not found.");
+      }
+    }, 500);
+  };
+
+  const findAndClickCancelButton = () => {
+    const btns = document.getElementsByTagName("button");
+    const length = btns.length;
+
+    console.log("backIcon cancel btn list====> ", btns);
+
+    for (let i = 0; i < length; i++) {
+      const currentBtn = btns[i];
+
+      if (findCancelBtn(currentBtn)) {
+        currentBtn.click();
+      }
     }
-  }, 500);
-};
+  };
 
-const findAndClickCancelButton = () => {
-  const btns = document.getElementsByTagName("button");
-  const length = btns.length;
-
-  console.log("backIcon cancel btn list====> ", btns);
-
-  for (let i = 0; i < length; i++) {
-    const currentBtn = btns[i];
-
-    if (findCancelBtn(currentBtn)) {
-      currentBtn.click();
+  const findCancelBtn = (btn) => {
+    if (!btn) {
+      return false;
     }
-  }
-};
 
-const findCancelBtn = (btn) => {
-  if (!btn) {
-    return false;
-  }
+    if (btn.nodeType === 3 && btn.textContent === "Cancel and Go Back") {
+      return true;
+    }
 
-  if (btn.nodeType === 3 && btn.textContent === "Cancel and Go Back") {
-    return true;
-  }
+    return findCancelBtn(btn.firstChild);
+  };
 
-  return findCancelBtn(btn.firstChild);
-};
-
-findBackIcon();
+  findBackIcon();
+})();
